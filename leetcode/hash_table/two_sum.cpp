a18bfed97dc2730e04117a460bab1fb42cf1730c
yeonhx03 <yeonhx915@gmail.com>
2026-08-29 22:09:19 +0900

    two_sum.cpp 2차 풀이

--- leetcode/hash_table/two_sum.cpp
+++ leetcode/hash_table/two_sum.cpp
@@ -1,16 +1,42 @@
+// 260828 1차 풀이 - 이중 반복문, 시간복잡도: O(n)
+
+// #include <vector>
+
+// using namespace std;
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         for(int i=0; i<nums.size(); i++ ){
+//             for(int j=i+1; j<nums.size(); j++){
+//                 if(nums[j]==(target-nums[i])){
+//                     return {i,j};
+//                 }
+//             }
+//         }
+//     }
+// };
+
+// 260829 2차 풀이 - hash table, unordered_map
+
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> seen;
+        
         for(int i=0; i<nums.size(); i++ ){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[j]==(target-nums[i])){
-                    return {i,j};
-                }
+            int need = target - nums[i];
+            if(seen.find(need) != seen.end()){
+                return {seen[need],i};
             }
+            seen[nums[i]]=i;
+
         }
+        return {};
     }
 };
